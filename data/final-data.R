f4072268326260568850d2200c14e7a37addbc46
nparmentier <naomiparmentier2001@gmail.com>
2025-02-13 22:06:31 +0100

    data explorationa added

--- data/final-data.R
+++ data/final-data.R
@@ -1,2 +1,8 @@
 "In this file we run the code to open the final data set that'll be used for the rest of this project."
-yelp_data <- read_csv("yelp_final.csv")
+
+yelp_id <- "1_zEeMXNrsxDwcHfCTYxrba66eygPL0r_"
+yelp_url <- paste0("https://drive.google.com/uc?id=", yelp_id)
+
+
+# Download the file from Google Drive
+yelp_data <- read_csv(yelp_url)
