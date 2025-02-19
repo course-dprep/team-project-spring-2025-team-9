3b110681fb9967518116780bf6ab725456db335d
Mitsal Athaya <miya@Miyas-MacBook-Air.local>
2025-02-19 20:28:37 +0100

    remove unncessary spaces

--- data/download-data.R
+++ data/download-data.R
@@ -6,23 +6,16 @@ user_url <- paste0("https://drive.google.com/uc?id=", user_id)
 business_url <- paste0("https://drive.google.com/uc?id=", business_id)
 review_url <- paste0("https://drive.google.com/uc?id=", review_id)
 
-"When running the next line of code, you will be required to authenticate your account via Google. 
-Please enter 1 in the console to do so. This needs to be done due to the size of the file exceeding 
-100MB, which then enables the virus scanner on files uploaded through Google Drive. Once this is 
-finished, the file will be loaded locally on your device."
+"When running the next line of code, you will be required to authenticate your account via Google. Please enter 1 in the console to do so. This needs to be done due to the size of the file exceeding 100MB, which then enables the virus scanner on files uploaded through Google Drive. Once this is finished, the file will be loaded locally on your device."
 
 # Download the file from Google Drive
 drive_download(as_id(review_id), path = "reviews.csv", overwrite = TRUE)
 drive_download(as_id(user_id), path = "users.csv", overwrite = TRUE)
 
 # Reading CSV files
-"As mentioned previously, the size for the data set 'review' and 'users' are over the maximum megabytes 
-(100 MB) to be loaded without going through the Google Drive virus scan, therefore, it may take at least
-45 minutes to load up the files. Following up, we will then create a new, smaller sample from the larger
-data set which will be seen later in this file." 
+"As mentioned previously, the size for the data set 'review' and 'users' are over the maximum megabytes (100 MB) to be loaded without going through the Google Drive virus scan, therefore, it may take at least 45 minutes to load up the files. Following up, we will then create a new, smaller sample from the larger data set which will be seen later in this file." 
 
-"Note: the data set 'business' is under 100 MB so, 
-it can be loaded directly from the Google Drive URL through its ID."
+"Note: the data set 'business' is under 100 MB so, it can be loaded directly from the Google Drive URL through its ID."
 
 # Read the large file using fread (faster for large files)
 reviews <- read_csv("reviews.csv")
